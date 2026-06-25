0b2290d22cad5c87f5ac37b8f99ac77d0c6923b8
akshayjs1998 <akshayjs1998@gmail.com>
2026-06-25 14:07:39 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,25 +1,5 @@
-const CACHE = 'lyffin-listing-v2';
-const FILES = [
-  '/lyffin-listing/index.html',
-  '/lyffin-listing/manifest.json',
-  '/lyffin-listing/icon-192.png',
-  '/lyffin-listing/icon-512.png'
-];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
-  );
-});
+const CACHE = 'lyffin-listing-v3';
+const FILES = ['/lyffin-listing/index.html','/lyffin-listing/manifest.json','/lyffin-listing/icon-192.png','/lyffin-listing/icon-512.png'];
+self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES))); self.skipWaiting(); });
+self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))); self.clients.claim(); });
+self.addEventListener('fetch', e => { e.respondWith(fetch(e.request).catch(() => caches.match(e.request))); });
